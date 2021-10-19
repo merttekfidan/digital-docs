a0633514dbcc79a2ca8b1c31cea8801247b1978e
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-10-19 22:59:32 +0200

    Set route

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const documentRouter = require('./routes/documentRoutes')
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+app.use('/api/v1/documents',documentRouter)
 
 module.exports = app
