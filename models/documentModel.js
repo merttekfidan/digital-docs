b1b09547128b7a140d0798f02f1c4c56a0d81cc6
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-10-20 22:16:32 +0200

    Create model

--- models/documentModel.js
+++ models/documentModel.js
@@ -1,37 +1,37 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const docsSchema = mongoose.Schema({
-    name:{
-        type:String,
-        require:[true, 'A Doc must have a name'],
-    },
-    sentBy:{
-        type:String,
-    },
-    createdAt:{
-        type:Date,
-        default:Date.now(),
-    },
-    sentAt:{
-        type:Date,
-    },
-    category:{
-        type:String
-    },
-    images:[String],
-    description : {
-        type:String,
-    },
-    original:{
-        type:String,
-        enum:['mail','document',null]
-    },
-    sentTo:{
-        type:String
-    },
-    pdfUrl:{
-        type:String
-    }
-})
+  name: {
+    type: String,
+    required: [true, 'A Doc must have a name'],
+  },
+  sentBy: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  sentAt: {
+    type: Date,
+  },
+  category: {
+    type: String,
+  },
+  images: [String],
+  description: {
+    type: String,
+  },
+  original: {
+    type: String,
+    enum: ['mail', 'document', null],
+  },
+  sentTo: {
+    type: String,
+  },
+  pdfUrl: {
+    type: String,
+  },
+});
 
-const Document = mongoose.model('Document',docsSchema);
-module.exports = Document
+const Document = mongoose.model('Document', docsSchema);
+module.exports = Document;
