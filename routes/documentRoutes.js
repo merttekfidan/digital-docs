e74318cde5aed11cfadbc0bf949b693bf358d476
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-10-20 22:15:37 +0200

    Create model

---
+++ routes/documentRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const documentController = require('./../controllers/documentController');
+
+const router = express.Router();
+
+router.route('/').get(documentController.getAllDocuments).post(documentController.createDocument);
+router
+  .route('/:id')
+  .get(documentController.getOneDocument)
+  .patch(documentController.updateDocument)
+  .delete(documentController.deleteDocument);
+module.exports = router;
