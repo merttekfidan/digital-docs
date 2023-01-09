195a3b0dd1d08487702152338b4afa4f9bb159a1
Merdjan Tekfidan <merttekfidan@gmail.com>
2023-01-09 02:42:56 +0100

    Added and Modified new columns

---
+++ models/categoryModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+/*
+    name,
+    images : ['url'] 
+*/
+const categorySchema = mongoose.Schema({
+  customName: {
+    type: String,
+  },
+  images: [String]
+});
+
+const Category = mongoose.model('Category', categorySchema);
+module.exports = Category;
