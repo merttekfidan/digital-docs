dca44cacd5d629c6af22fe510d29e88e77024f17
Merdjan Tekfidan <merttekfidan@gmail.com>
2023-01-09 02:43:19 +0100

    Create Models

--- models/documentModel.js
+++ models/documentModel.js
@@ -1,6 +1,23 @@
 const mongoose = require('mongoose');
+/*
+  Custom Name,
+  Sent By,
+  created At - today,
+  Sent At - by destination point,
+  Expires At,
+  categoryId -  [categoryid],
+  Thumbnail - [],
+  Comment,
+  Original Location,
+  Pdf Url [url],
+  Type,
+  Keywords - [],
+  Title,
+  Permissions - Person
+
+*/
 const docsSchema = mongoose.Schema({
-  name: {
+  customName: {
     type: String,
     required: [true, 'A Doc must have a name'],
   },
@@ -14,24 +31,59 @@ const docsSchema = mongoose.Schema({
   sentAt: {
     type: Date,
   },
-  category: {
-    type: String,
+  expiresAt:Date,
+  categoryId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Category',
   },
-  images: [String],
-  description: {
-    type: String,
-  },
-  original: {
-    type: String,
-    enum: ['mail', 'document', null],
+  thumbnailUrl: String,
+  images:[String],
+  comment:String,
+  originalLocation: {
+    type:String,
   },
   sentTo: {
     type: String,
   },
-  pdfUrl: {
-    type: String,
+  pdfUrl: [String],
+  type: String,
+  keywords:[String],
+  title:String,
+  permission:{
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
   },
 });
 
 const Document = mongoose.model('Document', docsSchema);
 module.exports = Document;
+
+
+
+/*
+        Example Data
+
+            "customName": "Elektrik faturası",
+            "sentBy": "Elektrik İdaresi",
+            "createdAt": "2023-01-09T01:39:51.009Z",
+            "sentAt": "2021-01-07T23:52:37.128Z",
+            "thumbnailUrl": "urlstring",
+            "images": [
+                "test image url"
+            ],
+            "comment": "evin elektrik faturası",
+            "originalLocation": "Sarı klasör",
+            "sentTo": "Ev",
+            "pdfUrl": [
+                "url String"
+            ],
+            "type": "kağıt",
+            "keywords": [
+                "ev",
+                "fatura",
+                "kağıt"
+            ],
+            "title": " Bla Bla Kurumu Elektrik işleri Bla Bla",
+            "_id": "63bb70823b58a3808fd430d1",
+            "__v": 0
+  */
