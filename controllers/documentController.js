eeacdbdfb006b25b06ff27c86a7148d95e2e07d2
Merdjan Tekfidan <merttekfidan@gmail.com>
2023-01-24 21:53:35 +0100

    fix prettier and eslint

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -1,17 +1,17 @@
-const multer = require('multer');
-var path = require('path');
-const AppError = require('./../utils/appError');
-const catchAsync = require('./../utils/catchAsync');
-const Document = require('./../models/documentModel');
+const multer = require("multer");
+var path = require("path");
+const AppError = require("./../utils/appError");
+const catchAsync = require("./../utils/catchAsync");
+const Document = require("./../models/documentModel");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads');
+    cb(null, "./uploads");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = `${Date.now()}-${path.extname(file.originalname)}`;
     //dosya uzantısını otomatik çek
-    cb(null, file.fieldname + '-' + uniqueSuffix);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
   },
 });
 
@@ -21,9 +21,9 @@ const upload = multer({ storage: storage });
 //  req.files['gallery'] -> Array
 
 exports.uploadFiles = upload.fields([
-  { name: 'images', maxCount: 10 },
-  { name: 'thumbnailUrl', maxCount: 1 },
-  { name: 'pdfUrl', maxCount: 10 },
+  { name: "images", maxCount: 10 },
+  { name: "thumbnailUrl", maxCount: 1 },
+  { name: "pdfUrl", maxCount: 10 },
 ]);
 
 exports.uploadDocImages = catchAsync(async (req, res, next) => {
@@ -33,17 +33,17 @@ exports.uploadDocImages = catchAsync(async (req, res, next) => {
 
   // images
   if (req.files.images) {
-    req.files['images'].forEach((element) => {
+    req.files["images"].forEach((element) => {
       req.body.images.push(element.path);
     });
   }
   if (req.files.thumbnailUrl) {
-    req.files['thumbnailUrl'].forEach((element) => {
+    req.files["thumbnailUrl"].forEach((element) => {
       req.body.thumbnailUrl.push(element.path);
     });
   }
   if (req.files.pdfUrl) {
-    req.files['pdfUrl'].forEach((element) => {
+    req.files["pdfUrl"].forEach((element) => {
       req.body.pdfUrl.push(element.path);
     });
   }
@@ -53,11 +53,11 @@ exports.uploadDocImages = catchAsync(async (req, res, next) => {
   // pdfUrl must be written???
 
   req.body.images.forEach((e) => console.log(e));
-  console.log('req.body.images : ', req.body.images);
-  console.log('req.body.pdf : ', req.body.pdfUrl);
-  console.log('req.body.thumbnailUrl : ', req.body.thumbnailUrl);
+  console.log("req.body.images : ", req.body.images);
+  console.log("req.body.pdf : ", req.body.pdfUrl);
+  console.log("req.body.thumbnailUrl : ", req.body.thumbnailUrl);
   res.status(201).json({
-    status: 'success',
+    status: "success",
   });
 });
 
@@ -65,7 +65,7 @@ exports.createDocument = catchAsync(async (req, res, next) => {
   //Validations should be added
   const documents = await Document.create(req.body);
   res.status(201).json({
-    status: 'success',
+    status: "success",
     data: { data: documents },
   });
 });
@@ -73,7 +73,7 @@ exports.createDocument = catchAsync(async (req, res, next) => {
 exports.getAllDocuments = catchAsync(async (req, res, next) => {
   const documents = await Document.find();
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: { data: documents },
   });
 });
@@ -81,9 +81,9 @@ exports.getAllDocuments = catchAsync(async (req, res, next) => {
 exports.getOneDocument = catchAsync(async (req, res, next) => {
   const document = await Document.findById(req.params.id);
   if (!document)
-    return next(new AppError('No document found with that ID!', 404));
+    return next(new AppError("No document found with that ID!", 404));
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: { data: document },
   });
 });
@@ -94,9 +94,9 @@ exports.updateDocument = catchAsync(async (req, res, next) => {
     new: true,
   });
   if (!documents)
-    return next(new AppError('No document found with that ID!', 404));
+    return next(new AppError("No document found with that ID!", 404));
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: { data: documents },
   });
 });
@@ -104,9 +104,9 @@ exports.updateDocument = catchAsync(async (req, res, next) => {
 exports.deleteDocument = catchAsync(async (req, res, next) => {
   const documents = await Document.findByIdAndDelete(req.params.id);
   if (!documents)
-    return next(new AppError('No document found with that ID!', 404));
+    return next(new AppError("No document found with that ID!", 404));
   res.status(204).json({
-    status: 'success',
+    status: "success",
     data: null,
   });
 });
@@ -123,7 +123,7 @@ exports.filterDate = catchAsync(async (req, res, next) => {
     },
   ]);
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: { data: documents },
   });
 });
